004c77db1d22578ea2a9a1bc47e401e9ee9bc171
SourabhGurwani <sonusourabh2411@gmail.com>
2025-07-10 16:04:50 +0530

    cors update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import cors from "cors";
 import userRouter from "./routes/userRoute.js";
 dotenv.config()
+
+
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const dbuser = encodeURIComponent(process.env.DBUSER)
